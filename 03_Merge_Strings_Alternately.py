232c95919f35aa6717bf0a18040f40be2d9acb2c
Abdi Dabala <abdid.yadata@gmail.com>
2025-08-20 16:06:14 -0700

    Merge Strings Alternately

--- 03_Merge_Strings_Alternately.py
+++ 03_Merge_Strings_Alternately.py
@@ -10,3 +10,17 @@ class Solution:
                 merged.append(word2[i])
             i +=1
         return ''.join(merged)
+
+'''
+ Time complexity: O(n)
+    - where n is the length of the longer string between word1 and word2.
+    - the while loop iterates through bothe strings simultaneously and the
+      the number of iterations is determined by the maximum length of the two strings.
+    - the .join() operation aslo takes O(n) time, making the overall time complexity O(n)
+
+Space complexity: O(n)
+   - where n is the length of the longer string. 
+   - the space used to store the merged list, which can contain at most n charchters
+     (the sum of lengths of word1 and word2), but effectively limited by the longer string due to the alternating pattern
+     - the output string also requires O(n) space, so the total space complexity is O(n)
+'''
