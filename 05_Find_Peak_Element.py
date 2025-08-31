c92399883ec8b78f8d148dc6d3c4db63af4e727a
Abdi Dabala <abdid.yadata@gmail.com>
2025-08-31 03:18:42 -0700

    162. Find Peak Element

---
+++ 05_Find_Peak_Element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+
+'''
+Complexity
+
+   Time: O(log n) (binary search halves search space each step).
+
+   Space: O(1) (just pointers)
+'''
